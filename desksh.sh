0372126b3afd1b1d3a9cafec5f4007f6034a0d01
Cristian Leyes <cristiandley@users.noreply.github.com>
2014-03-09 05:31:55 -0200

    Update desksh.sh

--- desksh.sh
+++ desksh.sh
@@ -7,7 +7,7 @@ sudo apt-get update -y
 sudo apt-get install sublime-text-installer -y
 sudo apt-get install curl -y
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 -y
-sudo echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list -y
+sudo echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update -y
 sudo apt-get install mongodb-10gen -y
 sudo service mongodb start -y
