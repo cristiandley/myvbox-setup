da8322117857f168ebdff33e69cb3335d62e8386
Cristian Leyes <cristiandley@users.noreply.github.com>
2014-03-09 04:20:13 -0200

    Update desksh.sh
    added sublime text

--- desksh.sh
+++ desksh.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
+sudo apt-get update -y
+sudo apt-get install sublime-text-installer -y
 sudo apt-get install curl -y
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 -y
 sudo echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list -y
@@ -12,4 +15,5 @@ sudo apt-get update -y
 sudo apt-get install nodejs -y
 curl https://install.meteor.com | /bin/sh
 sudo npm install -g meteorite
+
 
